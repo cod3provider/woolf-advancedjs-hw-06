5913e32b8f4237a7717af096fdf02a7eab633d5b
Ilya Bondarchuk <cod3provider@gmail.com>
2023-12-09 15:16:00 +0200

    task 2-3 done

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,8 +3,10 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
+function merge<T extends object, U extends object> (objA: T, objB: U): T & U {
   return Object.assign(objA, objB);
 }
+
+merge({name: 'Tom'}, {position: 5})
 
 export {}
