e91b7653b121f86eba0e00a6ae0546333593c820
Ilya Bondarchuk <cod3provider@gmail.com>
2023-12-09 15:16:31 +0200

    task 2-4 done

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,16 +2,23 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+interface Props {
+  title: string;
+}
 
+class Component<T extends Props> {
+  constructor (public props: T) {
+    console.log(this.props);
   }
 }
 
-class Page extends Component {
+class Page extends Component <Props>{
   pageInfo () {
     console.log(this.props.title);
   }
 }
+
+const page = new Page({title: 'Title'});
+page.pageInfo();
 
 export {};
